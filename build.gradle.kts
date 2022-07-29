51b69df7e8363d28657c30ce6b81137dbf439fc4
Fei Chen <fei.chen@okta.com>
2022-07-28 20:21:35 -0700

    Add sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,19 @@ plugins {
     id("org.jetbrains.dokka") version ("1.6.10") apply false
     id("com.google.gms.google-services") version ("4.3.13") apply false
     id("org.jetbrains.kotlinx.kover") version "0.5.1" apply false
+    id("org.sonarqube") version "3.4.0.2513" apply true
 }
 
 task<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+
+sonarqube {
+    val properties = java.util.Properties()
+    project.rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use { properties.load(it) }
+    properties {
+        property("sonar.host.url", properties.getProperty("sonar.host.url") ?: System.getenv("SONAR_HOST_URL") ?: "")
+        property("sonar.projectKey", properties.getProperty("sonar.project.key") ?: System.getenv("SONAR_PROJECT_KEY") ?: "")
+        property("sonar.coverage.jacoco.xmlReportPaths", properties.getProperty("sonar.report.paths") ?: System.getenv("SONAR_REPORT_PATHS") ?: "")
+    }
 }
