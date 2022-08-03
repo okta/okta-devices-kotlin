3847eae74cb379fb83b16115d3736693b224f254
Fei Chen <fei.chen@okta.com>
2022-08-03 14:28:32 -0700

    Remove DeviceResult from PushChallenge (#14)
    OKTA-520746 Remove DeviceResult from PushChallenge
    
    Use Result directly instead of converting.
    Update to Java11. Remove unused testImplementation

--- push-sample-app/build.gradle.kts
+++ push-sample-app/build.gradle.kts
@@ -50,12 +50,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     buildFeatures {
@@ -77,7 +77,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.biometric:biometric:1.2.0-alpha04")
-    implementation("androidx.activity:activity-compose:1.5.0")
+    implementation("androidx.activity:activity-compose:1.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${Version.archLifecycleVersion}")
     implementation("androidx.compose.material:material:${Version.compose}")
     implementation("androidx.compose.ui:ui:${Version.compose}")
