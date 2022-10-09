4a21f35b221407cfb52e548801f2bb0dd7d05c54
Fei Chen <fei.chen@okta.com>
2022-10-08 18:56:36 -0700

    Update libraries
    Fix tests due to library change.
    Suppress protobuf owasp error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.2.2" apply false
-    id("com.android.library") version "7.2.2" apply false
+    id("com.android.application") version "7.3.0" apply false
+    id("com.android.library") version "7.3.0" apply false
     id("org.jetbrains.kotlin.android") version Version.kotlin apply false
     id("org.jetbrains.dokka") version "1.6.10" apply false
-    id("com.google.gms.google-services") version "4.3.13" apply false
-    id("org.jetbrains.kotlinx.kover") version "0.5.1" apply false
+    id("com.google.gms.google-services") version "4.3.14" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.6.1" apply false
     id("org.sonarqube") version "3.4.0.2513" apply true
     id("io.gitlab.arturbosch.detekt") version "1.21.0" apply false
 }
--- push-sample-app/build.gradle.kts
+++ push-sample-app/build.gradle.kts
@@ -74,10 +74,10 @@ dependencies {
     implementation("com.okta.kotlin:oauth2")
     implementation("com.okta.kotlin:web-authentication-ui")
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("androidx.biometric:biometric:1.2.0-alpha04")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation("androidx.activity:activity-compose:1.6.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${Version.archLifecycleVersion}")
     implementation("androidx.compose.material:material:${Version.compose}")
     implementation("androidx.compose.ui:ui:${Version.compose}")
@@ -90,7 +90,7 @@ dependencies {
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:30.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:30.5.0"))
     implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha03")
 }
