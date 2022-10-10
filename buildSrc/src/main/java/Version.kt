f897a7170e0609ae1ef8a17863be52acd7a1a3be
Fei Chen <fei.chen@okta.com>
2022-10-10 10:56:47 -0700

    Update libraries (#29)
    OKTA-539906 Update libraries
    
    Fix tests due to library change.
    Suppress protobuf owasp error

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -2,16 +2,16 @@
  * Version variables
  */
 object Version {
-    const val kotlin = "1.7.0"
-    const val kotlinSerialization = "1.3.3"
+    const val kotlin = "1.7.20"
+    const val kotlinSerialization = "1.4.0"
     const val coroutine = "1.6.4"
     const val room = "2.4.3"
     const val extJunit = "1.1.3"
     const val archLifecycleVersion = "2.5.1"
-    const val compose = "1.3.0-alpha02"
-    const val composeCompiler = "1.2.0"
-    const val devicesAuthenticator = "0.0.1"
+    const val compose = "1.3.0-rc01"
+    const val composeCompiler = "1.3.2"
+    const val devicesAuthenticator = "0.0.2-SNAPSHOT"
     const val devicesCore = "0.0.2-SNAPSHOT"
-    const val devicesStorage = "0.0.1"
-    const val devicesFakeServer = "0.0.1"
+    const val devicesStorage = "0.0.2-SNAPSHOT"
+    const val devicesFakeServer = "0.0.2-SNAPSHOT"
 }
