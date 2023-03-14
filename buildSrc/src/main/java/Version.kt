5dc78a8570589697f43c76d58df1c876c2a18884
Tommy Wong <tommy.wong@okta.com>
2023-03-14 17:46:16 -0400

    Update devices sdk version

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -10,8 +10,8 @@ object Version {
     const val archLifecycleVersion = "2.5.1"
     const val compose = "1.4.0-beta01"
     const val composeCompiler = "1.4.3"
-    const val devicesAuthenticator = "0.0.7-SNAPSHOT"
-    const val devicesCore = "0.0.7-SNAPSHOT"
-    const val devicesStorage = "0.0.7-SNAPSHOT"
-    const val devicesFakeServer = "0.0.7-SNAPSHOT"
+    const val devicesAuthenticator = "0.0.8"
+    const val devicesCore = "0.0.8"
+    const val devicesStorage = "0.0.8"
+    const val devicesFakeServer = "0.0.8"
 }
