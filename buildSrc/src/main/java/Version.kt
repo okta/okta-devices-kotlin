409dd04b6bd4c0fb93287e67c212c9784c92320b
Fei Chen <fei.chen@okta.com>
2023-09-11 16:02:12 -0700

    Update libraries and gradle

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -2,14 +2,14 @@
  * Version variables
  */
 object Version {
-    const val kotlin = "1.8.22"
-    const val kotlinSerialization = "1.5.1"
-    const val coroutine = "1.7.1"
+    const val kotlin = "1.9.10"
+    const val kotlinSerialization = "1.6.0"
+    const val coroutine = "1.7.3"
     const val room = "2.5.2"
     const val extJunit = "1.1.5"
-    const val archLifecycleVersion = "2.6.1"
-    const val compose = "1.4.3"
-    const val composeCompiler = "1.4.8"
+    const val archLifecycleVersion = "2.6.2"
+    const val compose = "1.5.1"
+    const val composeCompiler = "1.5.3"
     const val devicesAuthenticator = "0.0.15"
     const val devicesCore = "0.0.15"
     const val devicesStorage = "0.0.15"
